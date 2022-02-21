27c548a09fc6fe0cc9e7da401cabda936f5d97b5
Max Thomson <info@mthomson.ca>
2022-02-20 22:55:12 -0800

    Add shutdown command

--- cogs/stage.py
+++ cogs/stage.py
@@ -2,6 +2,8 @@ import datetime
 import os
 
 import discord
+from discord.commands import SlashCommandGroup
+from discord.ext import commands
 from discord.ext.commands import Cog
 
 import config
@@ -16,22 +18,25 @@ class Stage(Cog):
         channel = self.bot.get_channel(config.stage_channel)
         await channel.purge(limit=10000)
 
-    @Cog.listener()
-    async def on_command_error(self, ctx, error):
-        embed = discord.Embed(title="Error")
-        embed.set_thumbnail(
-            url = discord.utils.get(ctx.guild.emojis, name="Deny").url
-        )
-        embed.add_field(
-            name="Output",
-            value=f"```python\n{error}```",
-            inline=False,
-        )
-        embed.set_footer(text=config.bot_name, icon_url=self.bot.user.avatar)
-        embed.timestamp = datetime.datetime.utcnow()
+    stage = SlashCommandGroup("stage", "StageBot commands")
 
-        await ctx.channel.send(embed=embed)
-        raise error
+    @stage.command(description="Shutdown StageBot")
+    async def shutdown(self, ctx: commands.Context):
+        await ctx.respond(embed=await self.shutdown_embed())
+        await self.bot.close()
+
+    async def shutdown_embed(self):
+        uptime = str(datetime.datetime.utcnow() - self.bot.start_time)[2:][:5]
+        embed = discord.Embed(title="StageBot Shutting Down")
+        embed.add_field(
+            name="Uptime",
+            value=f"```{uptime}```",
+            inline=True,
+        )
+        embed.set_footer(text=config.bot_name, icon_url=self.bot.user.avatar.url)
+        embed.timestamp = datetime.datetime.utcnow()
+        embed.color = discord.Color.dark_green()
+        return embed
 
 
 def setup(bot):
