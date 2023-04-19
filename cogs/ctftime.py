f8dbc35523725ba4d09488a3daf4a6679239914e
Erik McKelvey <43917961+Erik-McKelvey@users.noreply.github.com>
2023-04-18 20:47:10 -0700

    Reformatting and added colours

--- cogs/ctftime.py
+++ cogs/ctftime.py
@@ -21,22 +21,23 @@ class CFTTime(commands.Cog):
             events = fetch_upcoming_events(5)
         except requests.exceptions.HTTPError as ex:
             print(f"An error occurred when fetching event info: {ex}")
-            ctx.respond("Sorry, I'm having trouble fetching the upcoming events.")
+            await ctx.respond("Sorry, I'm having trouble fetching the upcoming events.")
             return
 
         # Create an embed for each event
         embeds = []
         for event in events:
-            embed = discord.Embed(title=event["title"], color=discord.Color.blue())
+            embed = discord.Embed(title=event["title"], color=consistentHash(event["id"]))
 
-            # Format start time
-            utctime = int(datetime.fromisoformat(event["start"]).timestamp())
-            formatted_start_time = f"<t:{utctime}:f>"
+            embed.add_field(name="Start", value=ISOToHammerTime(event["start"]))
+            embed.add_field(name="Finish", value=ISOToHammerTime(event["finish"]))
+            embed.add_field(name="", value="") # Empty field to make two columns
+            embed.add_field(name="CTF Website", value=event["url"])
+            embed.add_field(name="CTFtime", value=f'[{event["id"]}]({event["ctftime_url"]})')
+            embed.add_field(name="", value="") # Empty field to make the rows the same length
 
-            embed.add_field(name="Start time", value=formatted_start_time, inline=False)
-            embed.add_field(name="Website", value=event["url"], inline=False)
-            embed.add_field(name="Event id", value=event["id"], inline=False)
             embeds.append(embed)
+        
         await ctx.respond(embeds=embeds)
 
     @ctftime.command(
@@ -47,55 +48,40 @@ class CFTTime(commands.Cog):
             event = fetch_event_details(event_id)
         except requests.exceptions.HTTPError as ex:
             print(f"An error occurred when fetching event info: {ex}")
-            ctx.respond(
+            await ctx.respond(
                 "Sorry, I'm having trouble fetching the details for this event. Are you sure this is a valid event id?"
             )
             return
 
-        # Format times
-        utctime = int(datetime.fromisoformat(event["start"]).timestamp())
-        formatted_start_time = f"<t:{utctime}:f>"
-
-        utctime = int(datetime.fromisoformat(event["finish"]).timestamp())
-        formatted_finish_time = f"<t:{utctime}:f>"
-        # Format duration
-        formatted_duration = ""
-        if event["duration"]["days"] > 0:
-            formatted_duration += f'{event["duration"]["days"]} days '
-        if event["duration"]["hours"] > 0:
-            formatted_duration += f'{event["duration"]["hours"]} hours'
-
         embed = discord.Embed(
             title=event["title"],
             description=event["description"],
-            color=discord.Color.blue(),
+            color=consistentHash(event["id"])
         )
         embed.set_thumbnail(url=event["logo"])
 
-        if len(event["organizers"]) > 1:
-            for index, org in enumerate(event["organizers"]):
-                embed.add_field(name=f"Organizer {index}", value=org["name"])
-                embed.add_field(name=f"Organizer {index} Id", value=org["id"])
-        else:
-            embed.add_field(name=f"Organizer", value=event["organizers"][0]["name"])
-            embed.add_field(name=f"Organizer Id", value=event["organizers"][0]["id"])
+        # Format organizer
+        organizer = f'[{event["organizers"][0]["name"]}](https://ctftime.org/team/{event["organizers"][0]["id"]})'
 
-        embed.add_field(name=f"Start", value=formatted_start_time)
-        embed.add_field(name=f"Finish", value=formatted_finish_time)
-        embed.add_field(name=f"Duration", value=formatted_duration)
-        embed.add_field(name=f"Website", value=event["url"])
-        embed.add_field(name=f"Is votable now", value=str(event["is_votable_now"]))
+        embed.add_field(name="Start", value=ISOToHammerTime(event["start"]))
+        embed.add_field(name="Finish", value=ISOToHammerTime(event["finish"]))
+        embed.add_field(name="Weight", value=event["weight"])
+        embed.add_field(name="CTF Website", value=event["url"])
+        embed.add_field(name="CTFtime", value=f'[{event["id"]}]({event["ctftime_url"]})')
+        embed.add_field(name=f"Organizer", value=organizer)
         embed.add_field(name=f"Restrictions", value=event["restrictions"])
         embed.add_field(name=f"Format", value=event["format"])
         embed.add_field(name=f"Participants", value=str(event["participants"]))
-        embed.add_field(name=f"CTFtime URL", value=event["ctftime_url"])
-        if event["location"]:
-            embed.add_field(name=f"Location", value=event["location"])
-        if event["live_feed"]:
-            embed.add_field(name=f"Live feed", value=event["live_feed"])
-        embed.add_field(name=f"Public votable", value=str(event["public_votable"]))
         await ctx.respond(embed=embed)
 
+def consistentHash(txt):
+    return int(hex(hash(str(txt)))[3:9], 16)
+
+# Convert a string from ISO format to HammerTime format
+def ISOToHammerTime(time):
+    utctime = int(datetime.fromisoformat(time).timestamp())
+    formatted_time = f"<t:{utctime}:f>"
+    return formatted_time
 
 def setup(bot):
     bot.add_cog(CFTTime(bot))
