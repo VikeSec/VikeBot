165555b2f06d7c62c7aabd26d5a58d92cf5e9222
Erik McKelvey <43917961+Erik-McKelvey@users.noreply.github.com>
2023-04-20 02:06:26 -0700

    Changed to be message command

--- cogs/code.py
+++ cogs/code.py
@@ -1,4 +1,4 @@
-from discord.commands import SlashCommandGroup
+import discord
 from discord.ext import commands
 
 from pyston import PystonClient, File
@@ -8,35 +8,59 @@ class Code(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    code = SlashCommandGroup("code", "Code commands")
+    @discord.message_command(description="Execute code")
+    async def execute(self, ctx, message: discord.Message):
+        code_blocks = get_code_blocks_with_languages(message.content)
+        if len(code_blocks) == 0:
+            ctx.respond("This command must be used on a message containing a code block")
 
-    @code.command(description="Execute code")
-    async def execute(self, ctx, language: str, input: str):
-        client = PystonClient()
+        responses = []
+        for block in code_blocks:
+            try:
+                result = await execute_code(block["language"], block["code"])
+            except InvalidLanguage:
+                await ctx.respond("Invalid language")
+                return
+            except TooManyRequests:
+                await ctx.respond("Too many requests. Please slow down")
+                return
+            except (ExecutionError, InternalServerError, UnexpectedError):
+                await ctx.respond("An unknown error has occured ¯\_(ツ)_/¯")
+                return
 
-        language = convert_language_alias(language)
+            responses.append(f'```{block["language"]}\n{block["code"]}```\n```{str(result)}```')
 
-        # Check if language is valid
-        try:
-            await client.get_runtimes(language)
-        except InvalidLanguage:
-            await ctx.respond("Invalid language")
-            return
+        response = "\n".join(responses)
+        await ctx.respond(response)
 
-        # Execute code
-        try:
-            result = await client.execute(language, [File(input)])
-        except ExecutionError as ex:
-            result = ex
-        except TooManyRequests:
-            await ctx.respond("Too many requests. Please slow down")
-            return
-        except (InternalServerError, UnexpectedError):
-            await ctx.respond("An unknown error has occured ¯\_(ツ)_/¯")
+async def execute_code(language, code):
+    client = PystonClient()
 
-        await ctx.respond(f'```{language}\n{input}```\n```{result}```')
+    # Check if language is valid
+    language = convert_language_alias(language)
+    await client.get_runtimes(language) # Raises InvalidLanguage if language is invalid
 
-# Convert language to a format that works in both Piston and Discord code blocks 
+    # Execute code
+    result = await client.execute(language, [File(code)]) # Raises TooManyRequests, ExecutionError, InternalServerError or UnexpectedError
+    return result
+
+# Get code blocks and languages from a message string
+def get_code_blocks_with_languages(txt):
+    result = []
+
+    blocks = txt.split('```')
+    if len(blocks) < 3:
+        return result
+    
+    i = 1
+    while (i < len(blocks)-1):
+        lang_and_code = blocks[i].split("\n", 1)
+        result.append({"language": lang_and_code[0], "code": lang_and_code[1]})
+        i += 2
+    
+    return result
+
+# Convert language to a format that works in Piston
 def convert_language_alias(lang):
     if lang.lower() == 'c#':
         return 'csharp'
