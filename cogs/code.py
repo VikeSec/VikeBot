81965cfc1760a764e416b806898d627826c0e2db
Erik McKelvey <43917961+Erik-McKelvey@users.noreply.github.com>
2023-04-20 02:06:51 -0700

    Ran black

--- cogs/code.py
+++ cogs/code.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from pyston import PystonClient, File
 from pyston.exceptions import *
 
+
 class Code(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,7 +13,9 @@ class Code(commands.Cog):
     async def execute(self, ctx, message: discord.Message):
         code_blocks = get_code_blocks_with_languages(message.content)
         if len(code_blocks) == 0:
-            ctx.respond("This command must be used on a message containing a code block")
+            ctx.respond(
+                "This command must be used on a message containing a code block"
+            )
 
         responses = []
         for block in code_blocks:
@@ -28,45 +31,53 @@ class Code(commands.Cog):
                 await ctx.respond("An unknown error has occured ¯\_(ツ)_/¯")
                 return
 
-            responses.append(f'```{block["language"]}\n{block["code"]}```\n```{str(result)}```')
+            responses.append(
+                f'```{block["language"]}\n{block["code"]}```\n```{str(result)}```'
+            )
 
         response = "\n".join(responses)
         await ctx.respond(response)
+
 
 async def execute_code(language, code):
     client = PystonClient()
 
     # Check if language is valid
     language = convert_language_alias(language)
-    await client.get_runtimes(language) # Raises InvalidLanguage if language is invalid
+    await client.get_runtimes(language)  # Raises InvalidLanguage if language is invalid
 
     # Execute code
-    result = await client.execute(language, [File(code)]) # Raises TooManyRequests, ExecutionError, InternalServerError or UnexpectedError
+    result = await client.execute(
+        language, [File(code)]
+    )  # Raises TooManyRequests, ExecutionError, InternalServerError or UnexpectedError
     return result
+
 
 # Get code blocks and languages from a message string
 def get_code_blocks_with_languages(txt):
     result = []
 
-    blocks = txt.split('```')
+    blocks = txt.split("```")
     if len(blocks) < 3:
         return result
-    
+
     i = 1
-    while (i < len(blocks)-1):
+    while i < len(blocks) - 1:
         lang_and_code = blocks[i].split("\n", 1)
         result.append({"language": lang_and_code[0], "code": lang_and_code[1]})
         i += 2
-    
+
     return result
+
 
 # Convert language to a format that works in Piston
 def convert_language_alias(lang):
-    if lang.lower() == 'c#':
-        return 'csharp'
-    if lang.lower() == 'cplusplus':
-        return 'c++'
+    if lang.lower() == "c#":
+        return "csharp"
+    if lang.lower() == "cplusplus":
+        return "c++"
     return lang.lower()
+
 
 def setup(bot):
     bot.add_cog(Code(bot))
